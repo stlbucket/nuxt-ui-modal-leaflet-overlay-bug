a32411166d17b21ce57a9682f2515299112f79c2
Kevin Burkett <stlbucket@gmail.com>
2023-08-28 10:25:35 -0700

    init commit

---
+++ app.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="text-3xl h-screen flex flex-col">
+    <div class="flex">
+      Tacos
+    </div>
+    <div class="text-3xl h-screen flex justify-around">
+      <div class="flex bg-blue-400 grow">
+        <div class="flex items-start">
+          <UButton label="Open" @click="isOpen = true" />
+        </div>
+
+        <!-- DOESN'T WORK WHEN STYLED LOCALLY EITHER -->
+        <!-- <UModal 
+          v-model="isOpen"
+          :ui="{
+            modal: {
+              wrapper: 'z-100'
+            },
+          }"
+        >
+          <UCard>
+            <template #header>
+              This is a header
+            </template>
+            This is the content area where stuff gets chopped under leaflet.
+          </UCard>
+        </UModal> -->
+
+        <UModal 
+          v-model="isOpen"
+        >
+          <UCard>
+            <template #header>
+              This is a header
+            </template>
+            This is the content area where stuff gets chopped under leaflet.
+          </UCard>
+        </UModal>
+      </div>
+      <div class="flex bg-green-400 grow">
+        <LMap
+          ref="map"
+          :zoom="zoom"
+          :center="[47.21322, -1.559482]"
+        >
+          <LTileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+            layer-type="base"
+            name="OpenStreetMap"
+          />
+        </LMap>
+      </div>
+    </div>
+  </div>
+  <UModal></UModal>
+</template>
+
+<script setup>
+const isOpen = ref(false)
+const zoom = ref(6)
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxthq/ui',
+    'nuxt3-leaflet'
+  ],
+  devtools: { enabled: true }
+})
